c371dcea5d9dd574383e743f518f33eefa4025d3
Liam Brophy <liambrophy.design@gmail.com>
2024-12-03 15:52:44 -0500

    completed calc

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n ++;
+    return n;
+}
+
+function decrement(n) {
+    n --;
+    return n;
+}
+
+function makeInt(n) {
+    const parsedInt = parseInt(n, 10);
+
+    return parsedInt; 
+}
+
+function preserveDecimal(n) {
+    const preservedDec = parseFloat(n, 10);
+
+    return preservedDec;
+}
